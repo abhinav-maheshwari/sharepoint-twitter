ec4e15d1236344ff4b24be7c628dd0f77a4454f7
tsg@brickred.com <tsg@brickred.com@99bb0637-9624-9a89-5ed8-a6c350b6e4ae>
2010-11-24 15:25:41 +0000

    added assembly changes and manifest changes

--- trunk/2007/BrickRed.WebParts.Twitter/Properties/AssemblyInfo.cs
+++ trunk/2007/BrickRed.WebParts.Twitter/Properties/AssemblyInfo.cs
@@ -2,18 +2,20 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Security;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("BrickRed.WebParts.Twitter")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("This assembly installs twitter webpart on Sharepoint portal")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("BrickRed")]
 [assembly: AssemblyProduct("BrickRed.WebParts.Twitter")]
-[assembly: AssemblyCopyright("Copyright ©  2010")]
+[assembly: AssemblyCopyright("BrickRed Copyright ©  2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+[assembly: AllowPartiallyTrustedCallers]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
